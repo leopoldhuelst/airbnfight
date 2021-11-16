743a8f9c70e7829face505c6779fd9722ac8b151
Jonas Stannowski <jonas.stannowski@icloud.com>
2021-11-16 17:59:04 +0100

    finished login and dashboard

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,5 +1,6 @@
 class DashboardsController < ApplicationController
   def index
-
+    @arenas = policy_scope(current_fighter.arenas)
+    @fights = current_fighter.fights
   end
 end
--- app/models/fighter.rb
+++ app/models/fighter.rb
@@ -2,12 +2,9 @@ class Fighter < ApplicationRecord
   has_many :arenas, dependent: :destroy
   has_many :fights, dependent: :destroy
 
-  # validates :email, presence: true, uniqueness: true, format: { with: %r(?:[a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\]) }
-  # validates :password, presence: true
-
-#  validates :nickname, presence: true, uniqueness: true
- # validates :height, presence: true
-  #validates :weight, presence: true
+  validates :nickname, presence: true, uniqueness: true
+  validates :height, presence: true
+  validates :weight, presence: true
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
