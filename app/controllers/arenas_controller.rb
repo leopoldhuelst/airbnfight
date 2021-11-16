a2f2c92da5fca204a4b957135325024af1fb067d
Leopold Huelst <leopold.huelst@gmail.com>
2021-11-16 12:52:21 +0100

    done

--- app/controllers/arenas_controller.rb
+++ app/controllers/arenas_controller.rb
@@ -1,8 +1,11 @@
 class ArenasController < ApplicationController
   def index
+    @arenas = Arena.all
   end
 
   def show
+    id = params[:id]
+    @arena = Arena.find(id)
   end
 
   def new
@@ -12,11 +15,30 @@ class ArenasController < ApplicationController
   end
 
   def destroy
+    id = params[:id]
+    arena = Arena.find(id)
+    arena.destroy
+
+    redirect_to arenas_path
   end
 
   def edit
+    id = params[:id]
+    @arena = Arena.find(id)
   end
 
   def update
+    id = params[:id]
+    arena = Arena.find(id)
+    arena.update(strong_params)
+    arena.save
+
+    redirect_to arena_path(arena.id)
+  end
+
+  private
+
+  def strong_params
+    params.require(:arena).permit(:address, :description, :capacity)
   end
 end
