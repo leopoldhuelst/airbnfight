ceec3abbff8b0a39ccfd64b7fa4a560b0ace74a5
Benjamin Dohna <dohnabenjamin@gmail.com>
2021-11-18 13:11:39 +0100

    Added user flow hospitals

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_18_101927) do
+ActiveRecord::Schema.define(version: 2021_11_18_104048) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,6 +63,8 @@ ActiveRecord::Schema.define(version: 2021_11_18_101927) do
     t.text "address"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.float "latitude"
+    t.float "longitude"
   end
 
   add_foreign_key "arenas", "fighters"
