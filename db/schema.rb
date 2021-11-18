8b3a0c5965e7b28b457519a42a0bdb9e1a637809
Adam Kerr <sirpoy@hotmail.com>
2021-11-18 14:59:36 +0100

    added geocoding code

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_17_105719) do
+ActiveRecord::Schema.define(version: 2021_11_18_102152) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 2021_11_17_105719) do
     t.bigint "fighter_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.float "latitude"
+    t.float "longitude"
     t.index ["fighter_id"], name: "index_arenas_on_fighter_id"
   end
 
